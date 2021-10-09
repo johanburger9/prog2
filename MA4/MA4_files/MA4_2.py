311ade83128eba311fb131f53d41ed485d556cb9
johanburger9 <33667906+johanburger9@users.noreply.github.com>
2021-10-09 23:46:05 +0200

    Update MA4_2.py

--- MA4/MA4_files/MA4_2.py
+++ MA4/MA4_files/MA4_2.py
@@ -4,27 +4,27 @@ from integer import Integer
 from time import perf_counter as pc
 
 def fib_py(n):
-    if n <= 1:
-        return n
-    else:
-        return(fib_py(n-1) + fib_py(n-2))
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
 
 def main():
-    #f = Integer(5)
-    #print(f.get())
-    #f.set(7)
-    #print(f.get())
+	#f = Integer(5)
+	#print(f.get())
+	#f.set(7)
+	#print(f.get())
 
-    timings2 = []
+	timings2 = []
 	
-    for n in range(30, 46):
-	start2 = pc()
-	f = Integer(n)
-	end2 = pc()
-	timings2.append(f.fib())
-    plt.figure()
-    plt.plot(range(30, 46), timings2)
-	
+	for n in range(30, 46):
+		start2 = pc()
+		f = Integer(n)
+		end2 = pc()
+		timings2.append(f.fib())
+	plt.figure()
+	plt.plot(range(30, 46), timings2)
+
 
 if __name__ == '__main__':
-    main()
+	main()
